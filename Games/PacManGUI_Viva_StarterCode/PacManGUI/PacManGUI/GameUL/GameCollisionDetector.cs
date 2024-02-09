3343f51f2bf9bfe92a11f0c9116e5785aa969072
FurqanIshaq29 <muhammadfurqanishaq0@gmail.com>
2024-02-09 15:03:38 +0500

    Game Upload

---
+++ Games/PacManGUI_Viva_StarterCode/PacManGUI/PacManGUI/GameUL/GameCollisionDetector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PacMan.GameGL;
+namespace PacMan.GameUL
+{
+    class GameCollisionDetector
+    {
+      
+        public bool isGhostCollideWithPacMan(GameGhost ghost) {
+            bool flag = false;
+
+          
+            //Write your Code Here
+            
+            return flag;
+        }
+
+        public bool isPacManCollideWithPallet(GameCell potentialCell) {
+            bool flag = false;
+            //Write your Code Here
+            
+
+
+
+            return flag;
+
+        }
+    }
+}
---
+++ Games/PacManGUI_Viva_StarterCode/PacManGUI/PacManGUI/GameUL/GameGhostHorizontal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PacMan.GameGL;
+using System.Drawing;
+namespace PacMan.GameUL
+{
+    public class GameGhostHorizontal : GameGhost
+    {
+        private GameDirection direction = GameDirection.Left;
+
+        public GameGhostHorizontal(Image ghostImage, GameCell startCell)
+            : base(ghostImage)
+        {
+            base.CurrentCell = startCell;
+        }
+
+        public override void move(GameCell gameCell)
+        {
+            if (base.CurrentCell != null)
+            {
+                base.CurrentCell.setGameObject(Game.getBlankGameObject());
+            }
+
+            base.CurrentCell = gameCell;
+        }
+
+        public override GameCell nextCell()
+        {
+            GameCell gameCell = base.CurrentCell;
+            GameCell gameCell2 = base.CurrentCell.nextCell(direction);
+            if (gameCell2 == gameCell)
+            {
+                if (direction == GameDirection.Right)
+                {
+                    direction = GameDirection.Left;
+                }
+                else if (direction == GameDirection.Left)
+                {
+                    direction = GameDirection.Right;
+                }
+            }
+            else
+            {
+                gameCell = gameCell2;
+            }
+
+            return gameCell;
+        }
+    }
+}
